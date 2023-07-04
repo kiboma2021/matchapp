ae29dfd2a78327c3d40e4bb5bc78b21bd46ce7f4
kiboma <omayioben@gmail.com>
2023-07-04 08:19:38 +0300

    Add resourceful routing to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :companions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "companions#index"
+  resources :companions do
+    resources :posts
+  end
+
 end
