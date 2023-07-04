3eeb7e3565e7def14ca893433713acbd0fa985e3
kiboma <omayioben@gmail.com>
2023-07-04 08:07:58 +0300

    Validate age

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,4 +1,5 @@
 class Companion < ApplicationRecord
     validates :name, presence: true, uniqueness: true, length: { minimum: 3}
-    validates_presence_of :height, :bio, :age
+    validates :age, presence: true, length: { minimum:18 }
+    validates_presence_of :height, :bio
 end
