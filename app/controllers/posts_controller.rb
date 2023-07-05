89f19f43a867b8743370df7fbe8174fb3d3de389
kiboma <omayioben@gmail.com>
2023-07-05 08:43:16 +0300

    Create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
     before_action :get_companion
+    before_action :set_post, only: [:create, :destroy]
 
     def create
-        @posts = @companion.posts.create(post_params)
+        @post = @companion.posts.build(post_params)
+
+        respond_to do |format|
+            if @post.save
+              format.html { redirect_to companion_posts_url(@companion), notice: "Post was successfully created." }
+              format.json { render :show, status: :created, location: @companion }
+            else
+              format.html { render :companion/posts, status: :unprocessable_entity }
+              format.json { render json: @post.errors, status: :unprocessable_entity }
+            end
+          end
+
+
     end
 
     def destroy
@@ -15,6 +28,10 @@ class PostsController < ApplicationController
 
     def get_companion
         @companion =Companion.find(params[:companion_id])
+    end
+
+    def set_post
+        @post = @companion.posts.find(params[:id])
     end
 
     def post_params
